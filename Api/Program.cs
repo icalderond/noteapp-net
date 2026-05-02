e883d4cb868a1994e774a9a54ebbd7371a9c88fa
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-05-02 16:06:23 -0600

    Changes for test deploy

--- Api/Program.cs
+++ Api/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Boiling", "Horrible", "Unbearable", "Tropical", "Sizzling","Mexico"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Boiling", "Horrible", "Unbearable", "Tropical", "Sizzling","Mexico", "Vietnam"
 };
 
 app.MapGet("/weatherforecast", () =>
