9b504d48014fffde82d56b18d3d98dcc582b5671
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-05-05 07:30:38 -0600

    + postgresql conncetion

---
+++ Api/Models/Note.cs
@@ -0,0 +1,8 @@
+public class Note
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
