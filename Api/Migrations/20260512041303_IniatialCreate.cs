377ae3f7c4784b26cc211a51c984aea4c43317f8
Israel Calderon de la Cruz <icalderond@outlook.com>
2026-05-11 22:23:06 -0600

    + migration files

--- Api/Migrations/20260512041303_IniatialCreate.cs
+++ Api/Migrations/20260512041303_IniatialCreate.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Api.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class IniatialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
